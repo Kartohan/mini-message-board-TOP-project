6c1acc973872f02676c978038fad0fdbf553d935
Kartohan <Kartohan@gmail.com>
2022-10-15 19:35:19 +0300

    Init/index page/form page/styles

---
+++ routes/index.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+  {
+    text: "My first message here :)",
+    user: "Alex",
+    added: new Date(),
+  },
+  {
+    text: "What's up?",
+    user: "Carl",
+    added: new Date(),
+  },
+  {
+    text: "Sheeesh",
+    user: "Kyle",
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message Board", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", function (req, res, next) {
+  const { author, message } = req.body;
+  messages.push({ text: message, user: author, added: new Date() });
+  res.redirect("/");
+});
+module.exports = router;
